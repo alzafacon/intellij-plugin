f9bf07a29a2ffc9ba5f5b0bd257b8fd7799c6d1d
GitHub Action <action@github.com>
2023-02-28 21:21:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/alzafacon/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alzafacon/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alzafacon.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alzafacon.intellijplugin.MyBundle
 
 class MyApplicationService {
 
